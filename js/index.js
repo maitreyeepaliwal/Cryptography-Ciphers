dfb0c1539a3a9bc75138f0bc9e861c5a33b7c0a6
maitreyeepaliwal <65955491+maitreyeepaliwal@users.noreply.github.com>
2021-05-30 12:04:42 +0530

    Delete js directory

--- js/index.js
+++
@@ -1,5 +0,0 @@
-var item = document.querySelector('#myLogo');
-var description = document.querySelector('.indexText');
-
-setTimeout(function(){ item.classList.remove('hidden'); }, 500);
-setTimeout(function(){ description.classList.remove('invisible'); }, 1100);
